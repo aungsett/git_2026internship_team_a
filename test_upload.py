12e57d8493356177ef52b52b61c72e2f51cc4170
aditya-vm-123 <natsukikai1@gmail.com>
2026-02-22 21:46:22 +0530

    integrated cloudinary storage and verified it with real uploads

---
+++ test_upload.py
@@ -0,0 +1,15 @@
+from backend.app.services.storage_service import StorageService
+
+class DummyFile:
+  def __init__(self):
+    self.filename = "resume2.pdf"
+
+  def read(self):
+    return b"fake pdf content 2"
+  
+file = DummyFile()
+
+service = StorageService()
+url = service.upload_cv(file, 99999)
+
+print("Uploaded to:", url)
